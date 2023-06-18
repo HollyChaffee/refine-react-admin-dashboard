c0930189a340b3c7f25ee70ddefafc79d12faa96
Student 3 <hollydchaffee@gmail.com>
2023-06-18 18:25:59 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Authenticated, ErrorComponent, GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-
-import { notificationProvider } from "@refinedev/antd";
+import { notificationProvider, Layout } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
-
 import routerBindings, {
-  DocumentTitleHandler,
+  CatchAllNavigate,
+  NavigateToResource,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
-import dataProvider from "@refinedev/simple-rest";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { ColorModeContextProvider } from "./contexts/color-mode";
+import { DashboardOutlined } from "@ant-design/icons";
+import { DashboardPage } from "pages/Dashboard";
+//import { Header, Title, OffLayoutArea } from "components";
+import { authProvider } from "authProvider";
+import { AuthPage } from "pages/auth";
+import jsonServerDataProvider from "@refinedev/simple-rest";
 
 function App() {
+  const API_URL = "https://api.finefoods.refine.dev";
+  const dataProvider = jsonServerDataProvider(API_URL);
+
   return (
     <BrowserRouter>
       <GitHubBanner />
@@ -21,18 +28,97 @@ function App() {
           <Refine
             notificationProvider={notificationProvider}
             routerProvider={routerBindings}
-            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+            authProvider={authProvider}
+            dataProvider={dataProvider}
             options={{
               syncWithLocation: true,
               warnWhenUnsavedChanges: true,
             }}
+            resources={[
+              {
+                name: "dashboard",
+                list: "/",
+                meta: {
+                  label: "Dashboard",
+                  icon: <DashboardOutlined />,
+                },
+              },
+            ]}
           >
             <Routes>
-              <Route index element={<WelcomePage />} />
+              <Route
+                element={
+                  <Authenticated
+                    fallback={<CatchAllNavigate to="/login" />}
+                  >
+                   {/*} <Layout
+                      Header={Header}
+                      Title={Title}
+                      OffLayoutArea={OffLayoutArea}
+                    >
+                      <Outlet />
+                </Layout>*/}
+                  </Authenticated>
+                }
+              >
+                <Route index element={<DashboardPage />} />
+              </Route>
+
+              <Route
+                element={
+                  <Authenticated fallback={<Outlet />}>
+                    <NavigateToResource resource="dashboard" />
+                  </Authenticated>
+                }
+              >
+                <Route
+                  path="/login"
+                  element={
+                    <AuthPage
+                      type="login"
+                      formProps={{
+                        initialValues: {
+                          email: "demo@refine.dev",
+                          password: "demodemo",
+                        },
+                      }}
+                    />
+                  }
+                />
+                <Route
+                  path="/register"
+                  element={
+                    <AuthPage
+                      type="register"
+                      formProps={{
+                        initialValues: {
+                          email: "demo@refine.dev",
+                          password: "demodemo",
+                        },
+                      }}
+                    />
+                  }
+                />
+              </Route>
+
+              <Route
+                element={
+                  <Authenticated>
+                    {/*<Layout
+                      Header={Header}
+                      Title={Title}
+                      OffLayoutArea={OffLayoutArea}
+                    >
+                      <Outlet />
+                </Layout>*/}
+                  </Authenticated>
+                }
+              >
+                <Route path="*" element={<ErrorComponent />} />
+              </Route>
             </Routes>
             <RefineKbar />
             <UnsavedChangesNotifier />
-            <DocumentTitleHandler />
           </Refine>
         </ColorModeContextProvider>
       </RefineKbarProvider>
